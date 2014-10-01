749e3772e18b900869155b927201e6efb240e99d
Mikhail Novikov <freiksenet@gmail.com>
2014-10-01 10:19:47 +0300

    Test stubs and part of tests.

--- test/reititin.js
+++ test/reititin.js
@@ -1,10 +1,112 @@
 "use strict";
 
-describe("Router creation", function () {
-  it("must accept all the ways to define routes", function () {
-    var reititin = require('../reititin');
-    new reititin.Router();
+var _ = require('lodash');
+
+var routeDefs = [
+  // Route with named function, name is 'routes'
+  ['/route', function routes (match) {}],
+  // Route with explicit name, name is 'route', :id is parameter
+  ['/route/:id', 'route', function (match) {}],
+  // Route with url name, name is '/route/good'
+  ['/route/good', function (match) {}],
+  // Route with *splat, matches url fragment
+  ['/splat/*splat', function splat (match) {}],
+  // Route with (optional) fragment
+  ['/optional(/thing)', function option (match) {}]
+];
+
+var routeDefsWithDefault = [['*', function (match) {}]].concat(routeDefs);
+
+describe("Router", function () {
+  it("must accept all ways to construct routes", function () {
+    var Reititin = require('../reititin');
+    var router = new Reititin.Router(routeDefs);
+    var routerWithDefault = new Reititin.Router(routeDefsWithDefault);
+
+    expect(router.routeList).toEqual([
+      'routes', 'route', '/route/good', 'splat', 'option'
+    ]);
+    expect(_.keys(router.routes).length).toEqual(5);
+    expect(_.keys(router.names).length).toEqual(5);
+    expect(router.defaultRoute).toBeUndefined();
+
+    expect(routerWithDefault.routeList).toEqual([
+      'routes', 'route', '/route/good', 'splat', 'option'
+    ]);
+    expect(_.keys(routerWithDefault.routes).length).toEqual(5);
+    expect(_.keys(routerWithDefault.names).length).toEqual(5);
+    expect(routerWithDefault.defaultRoute).toBeDefined();
+  });
+
+  it("must match all the defined routes", function () {
+    var Reititin = require('../reititin');
+    var router = new Reititin.Router(routeDefs);
+    var routerWithDefault = new Reititin.Router(routeDefsWithDefault);
+
+    expect(router.match('/ueou')).toEqual(false);
+
+    expect(routerWithDefault.match('/ueoe')).toEqual({
+      name: 'default',
+      url: '/ueoe',
+      params: {},
+      query: {}
+    });
+
+    expect(router.match('/route')).toEqual({
+      name: 'routes',
+      url: '/route',
+      params: {},
+      query: {}
+    });
+
+    expect(router.match('/route/5')).toEqual({
+      name: 'route',
+      url: '/route/5',
+      params: {id: '5'},
+      query: {}
+    });
+
+    expect(router.match('/route/5?foo=baz&gar=brak')).toEqual({
+      name: 'route',
+      url: '/route/5?foo=baz&gar=brak',
+      params: {id: '5'},
+      query: {
+        foo: 'baz',
+        gar: 'brak'
+      }
+    });
+
+    expect(router.match('/splat/foo/bar/baz')).toEqual({
+      name: 'splat',
+      url: '/splat/foo/bar/baz',
+      params: {splat: 'foo/bar/baz'},
+      query: {}
+    });
+  });
+
+  it("must call callback when routing", function () {
     // TODO
-    expect(true).toBe(true);
+  });
+
+  it("must reverse the route from the url", function () {
+    // TODO
+  });
+
+  it("must be possible to add or remove a route", function () {
+    // TODO
+  });
+});
+
+describe("HashRouter", function () {
+  it("must bind and unbind window event", function () {
+    // TODO
+  });
+
+  it("must react to window changes", function () {
+    // TODO
+  });
+
+  it("must navigate to the reversed url", function () {
+    // TODO
   });
 });
